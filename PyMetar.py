408c810094d97bc3335eb431335edb1e47305f38
oe2mal <oe2mal@oevsv.at>
2021-08-07 17:40:34 +0200

    Solved problems on not existing pathes. They will be created now, if they do not exist

--- PyMetar.py
+++ PyMetar.py
@@ -4,7 +4,12 @@ import urllib.request
 
 # Path declaration
 cwd_path = Path('.')
-data_path = cwd_path/'data'/'stations'
+data_path = cwd_path/'data'
+stations_path = data_path/'stations'
+
+# Check if declared path exist, else create it 
+Path(data_path).mkdir(parents=True, exist_ok=True)
+Path(stations_path).mkdir(parents=True, exist_ok=True)
 
 # Station
 ICAO = input('Enter 4 letter ICAO-Code : ')
@@ -13,13 +18,15 @@ station = ICAO.upper() + '.TXT'
 # Download Url declaration
 URL_general='https://tgftp.nws.noaa.gov/data/observations/metar/'
 URL_station= URL_general+'stations/'+station
-URL_decoded= URL_general+'decoded/'+station
+
+# not now... maybe for later use?
+# URL_decoded= URL_general+'decoded/'+station
 
 # Download and save file
-urllib.request.urlretrieve(URL_station, data_path/station)
+urllib.request.urlretrieve(URL_station, stations_path/station)
 
 # Print file
-report = open(data_path/station, 'r')
+report = open(stations_path/station, 'r')
 for line in report:
     print(line)
 report.close()
